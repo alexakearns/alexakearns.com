816a56b9b82f6089a55d54264bc325cea3898daf
alexa.kearns <alexa.kearns@potatolondon.com>
2021-10-15 17:04:15 +0200

    Add Home view and Intro component

---
+++ src/components/Intro.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{my_name}}</h1>
+  <h3>{{job_title}}</h3>
+</template>
+
+<script>
+export default {
+  name: "Intro",
+  props: {
+    my_name: String,
+    job_title: String,
+  },
+}
+</script>
+
+<style>
+
+</style>
