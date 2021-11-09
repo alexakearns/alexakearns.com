57f562858f6b7457aded9f54d3dcb12b44bca473
alexa.kearns <alexa.kearns@potatolondon.com>
2021-11-09 11:39:32 +0000

    Create Social component. Add to ContactForm

---
+++ src/components/Social.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="social">
+    <h3 class="social__links"><a href="https://github.com/alexakearns">Github</a></h3>
+    <h3 class="social__links"><a href="https://www.linkedin.com/in/alexa-kearns/">LinkedIn</a></h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.social {
+  display: flex;
+  justify-content: center;
+}
+
+.social__links {
+  background: #7BE0AA;
+  border: 2px solid navy;
+  border-radius: 10px;
+  margin: 50px;
+  padding: 20px;
+}
+.social__links:hover {
+  background: #FF99A3;
+  border-radius: 50%;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,18 @@
 <template>
   <h1>Get in touch.</h1>
   <ContactForm />
+  <Social />
 </template>
 
 <script>
 import ContactForm from "@/components/ContactForm.vue";
+import Social from "@/components/Social.vue";
 
 export default {
   name: "Contact",
   components: {
     ContactForm,
+    Social,
   },
 };
 </script>
