aa537ae12e710499addf54e1148f8d9addb1d57f
alexa.kearns <alexa.kearns@potatolondon.com>
2021-11-09 11:53:35 +0000

    Add image to App background. Basic styling to nav

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,40 @@
 </template>
 
 <style>
+body {
+  background-color: #000;
+  margin: 0px;
+  overflow: hidden;
+  padding: 0px;
+}
+
 #app {
+  background-image: url(assets/portfolio-background_2.jpg);
+  background-position: center;
+  background-size: cover;
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  position: relative;
   text-align: center;
-  color: #2c3e50;
+  width: 100vw;
 }
 
 #nav {
   padding: 30px;
+  display: table;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+}
+
+#nav a:hover {
+  font-weight: bold;
+  color: pink;
 }
 
 #nav a.router-link-exact-active {
