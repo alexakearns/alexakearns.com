0c0b166bd155c2c3542bcad533a3fff006960d18
alexa.kearns <alexa.kearns@potatolondon.com>
2022-01-04 15:17:12 +0000

    Update contact form - still to submit info

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,16 +1,18 @@
 <template>
-  <form class="contact-form" @submit.prevent="onSubmit">
-    <label for="name">Name:</label>
-    <input id="name" v-model="name" />
-    <label for="email">Email:</label>
-    <input id="email" type="email" v-model="email" />
-    <label for="subject">Subject:</label>
-    <input id="subject" v-model="subject" />
-    <label for="message">Message:</label>
-    <input id="message" v-model="message" />
+  <div class="contact">
+    <div class="contact__container">
+      <form class="contact-form" @submit.prevent="onSubmit">
+        <div class="contact-form__2-up">
+          <input id="name" v-model="name" placeholder="Name" />
+          <input id="email" type="email" v-model="email" placeholder="Email" />
+        </div>
+        <input id="subject" v-model="subject" placeholder="Subject" />
+        <input id="message" v-model="message" placeholder="Message" />
 
-    <input class="contact-form__submit" type="submit" />
-  </form>
+        <input class="contact-form__submit" type="submit" />
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -43,8 +45,38 @@ export default {
 };
 </script>
 <style>
+.contact__container {
+  /* position: relative; */
+  display: flex;
+  justify-content: center;
+}
+
 .contact-form {
   display: grid;
   width: 250px;
+  /* position: absolute; */
+  /* left: 50%;
+  top: 50%; */
+}
+
+.contact-form__2-up {
+  display: flex;
+  margin-bottom: 5px;
+}
+
+.contact-form__2-up #name {
+  margin-right: 2.5px;
+}
+
+.contact-form__2-up #email {
+  margin-left: 2.5px;
+}
+
+.contact-form__2-up #message {
+  height: 50px;
+}
+
+.contact-form__submit {
+  margin: 15px;
 }
 </style>
